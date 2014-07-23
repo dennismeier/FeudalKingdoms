3cbab4810203b52957423a3c3a8b3ade8ff7a27b
dmeier54 <dennisraymeier@gmail.com>
2014-07-22 21:04:38 -0600

    Finally freaking worked!

--- PowerStates.R
+++ PowerStates.R
@@ -1,11 +1,10 @@
 powerstates<-function(){
 ## Read NAICS table
   naicstable<-read.csv("NAICS.csv", header=TRUE)
-  naics<-subset(naicstable, select=NAICS.id)
+  naics<-naicstable[,"NAICS.id"]
   naics<-as.character(naics)
 ## Read the BP table 
   mydata<-read.csv("BP_2012_00A1_with_ann.csv",header=TRUE)
 ##subset by the supplied NAICS vector
-  print(str(mydata))
-  power<-mydata[mydata$NAICS %in% naics, ]
+  power<-mydata[mydata$NAICS.id %in% naics,c("GEO.display.label","NAICS.id","NAICS.display.label","EMP")]
 }
